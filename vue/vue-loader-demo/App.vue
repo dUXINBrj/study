a3ca329d88361e1a103f19e9a75037ac5eb1a5f8
dUXINBrj <duxin.sky@gmail.com>
2017-10-19 15:55:38 +0800

    vue组件 路由

---
+++ vue/vue-loader-demo/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app">
+		<a v-link="{path:'/home'}">首页</a>
+		<a v-link="{path:'/news'}">新闻</a>
+		<router-view></router-view>
+	</div>
+</template>
+<script>
+	import Menu from './components/Menu.vue'
+
+	export default{
+		data(){
+			return {
+				msg:'welcome Vue ^_^'
+			}
+		},
+		methods:{
+			change(){
+				this.msg='wahaha'
+			}
+		},
+		components:{
+			'my-menu':Menu
+		}	
+	}
+</script>
+<style>
+	body{
+		background: #ccc
+	}
+</style>
