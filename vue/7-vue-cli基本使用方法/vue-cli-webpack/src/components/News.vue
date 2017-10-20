b6ee16b55decda3569ef1ef0cdc1f292e0422c53
dUXINBrj <duxin.sky@gmail.com>
2017-10-20 17:48:54 +0800

    vue-cli基本使用方法

---
+++ vue/7-vue-cli基本使用方法/vue-cli-webpack/src/components/News.vue
@@ -0,0 +1,17 @@
+<template>
+  <h4 @click="change">{{newsData}}</h2>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                newsData:'news1'
+            }
+        },
+        methods:{
+            change(){
+                this.newsData="news2"
+            }
+        }
+    }
+</script>
