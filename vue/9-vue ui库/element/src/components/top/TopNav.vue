071b6ebede13dac66aa109eb64b7b62f9b2a5af0
dUXINBrj <duxin.sky@gmail.com>
2017-11-20 18:33:11 +0800

    菜单与路由联动

--- vue/9-vue ui库/element/src/components/top/TopNav.vue
+++ vue/9-vue ui库/element/src/components/top/TopNav.vue
@@ -8,14 +8,8 @@
   background-color="#545c64"
   text-color="#fff"
   active-text-color="#ffd04b">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+  <el-menu-item index="/index/dashboard"><router-link to="/index/dashboard">dashboard</router-link></el-menu-item>
+  <el-menu-item index="/index/level1map"><router-link to="/index/level1map">地图</router-link></el-menu-item>
 </el-menu>
 	</div>
 </template>
@@ -24,8 +18,11 @@
       name:'TopNav',
       data() {
         return {
-          activeIndex: '1',
+          activeIndex: '/index/dashboard',
         };
+      },
+      mounted(){
+          this.activeIndex=this.$route.path;
       },
       methods: {
         handleSelect(key, keyPath) {
--- vue/9-vue ui库/element/src/main.js
+++ vue/9-vue ui库/element/src/main.js
@@ -11,14 +11,14 @@ Vue.use(ElementUI);
 Vue.use(vueRouter);
 
 //如果vuerouter需要多个配置 传参为对象时 routes文件内需要直接导出数组 而不是对象
-const router=new vueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes
+const router = new vueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes
 });
 
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+let vm = new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
 })
