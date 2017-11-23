dd01b30a82351b73b100d77e9aeaa87c103106f6
dUXINBrj <duxin.sky@gmail.com>
2017-11-23 18:33:26 +0800

    登录 过渡

--- vue/9-vue ui库/element/src/components/Container.vue
+++ vue/9-vue ui库/element/src/components/Container.vue
@@ -3,17 +3,28 @@
 		<el-container>
 		  <el-header>
         <el-row>
-          <el-col :span="2">1</el-col>
+          <el-col :span="2">LOGO</el-col>
           <el-col :span="20">
             <TopNav></TopNav>            
           </el-col>
-          <el-col :span="2">2</el-col>          
+          <el-col :span="2">
+            <el-dropdown trigger="click" @command="handleCommand">
+              <span class="el-dropdown-link">
+                您好 {{username}}<img class="header" src="../assets/img/img.jpg">
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="loginout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>    
+          </el-col>          
         </el-row>
         </el-header>
 		  <el-container>
 		    <el-container>
 		      <el-main>
+            <transition mode="out-in" enter-active-class="animated zoomInLeft" leave-active-class="animated zoomOutRight">
             <router-view></router-view>
+            </transition>
           </el-main>
 		      <el-footer>Footer</el-footer>
 		    </el-container>
@@ -25,26 +36,45 @@
 <script>
 import TopNav from './top/TopNav.vue'
 	export default{
-		name:'container',
+    name:'container',
+    data() {
+      return {
+          name: ''
+      }
+    },
+    computed:{
+      username(){
+          let username = localStorage.getItem('ms_username');
+          // if(!username){
+          //   this.$router.replace('/login');
+          // }else{
+          //   this.name=username
+          // }
+          return username ? username : this.name;
+      }
+    },
+    methods:{
+      handleCommand(command) {
+          if(command == 'loginout'){
+              localStorage.removeItem('ms_username')
+              this.$router.replace('/login');
+          }
+      }
+    },
     components:{
       TopNav
       }
 	}
 </script>
-<style>
+<style scoped>
 #container{
 	height: 100%;
 }
 	  .el-header, .el-footer {
-    background-color:rgb(84, 92, 100);;
-    color: #333;
+    background-color:#242f42;
+    color: #fff;
     text-align: center;
     line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
   }
   
   .el-main {
@@ -66,5 +96,15 @@ import TopNav from './top/TopNav.vue'
   
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
+  }
+
+  .el-dropdown{
+    color: #efefef;
+  }
+  .header{
+    width: 40px;
+    border-radius: 50%;
+    vertical-align: middle;
+    margin-left: 10px;
   }
 </style>
