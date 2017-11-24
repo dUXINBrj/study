c5b9fe8788155214d20a8031a47b004bec5a4ae2
dUXINBrj <duxin.sky@gmail.com>
2017-11-24 14:53:38 +0800

    修改element的引用方式为按需引用

--- vue/9-vue ui库/element/src/main.js
+++ vue/9-vue ui库/element/src/main.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vueRouter from 'vue-router'
-import ElementUI from 'element-ui'
+import {
+    Container,
+    Header,
+    Aside,
+    Main,
+    Footer,
+    Loading,
+    Tree,
+    Col,
+    Row,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    Menu,
+    MenuItem,
+    Input
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/common.css'
 import './assets/css/animate.css'
 
 import routes from './router.config.js'
 
-Vue.use(ElementUI);
 Vue.use(vueRouter);
+
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
+Vue.use(Loading);
+Vue.use(Tree);
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Container);
+Vue.use(Input);
 
 const router = new vueRouter({ routes });
 
