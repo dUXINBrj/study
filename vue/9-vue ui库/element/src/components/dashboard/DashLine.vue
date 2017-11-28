5cb2408c7f6a7c0f986286540badea82d728e9aa
dUXINBrj <duxin.sky@gmail.com>
2017-11-28 17:49:41 +0800

    增加echarts

--- vue/9-vue ui库/element/src/components/dashboard/DashLine.vue
+++ vue/9-vue ui库/element/src/components/dashboard/DashLine.vue
@@ -4,20 +4,41 @@
         <span>折线图</span>
         <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
         </div>
-        <div v-for="o in 4" :key="o" class="text item">
-        {{'列表内容 ' + o }}
+        <div class="text item" id="dashLine" ref="mychart">
+        
         </div>
     </el-card>
 </template>
 
 <script>
+    const echarts = require('echarts/lib/echarts');
+    require('echarts/lib/component/tooltip');
+    require('echarts/lib/component/title');
+    require("echarts/lib/chart/line");
     export default {
-        
+        mounted(){
+            this.initCharts();
+        },
+        data(){
+            return{
+               myChart:{} 
+            }
+        },
+        methods:{
+            initCharts(){
+                let dom = this.$refs.mychart;
+                this.myChart = echarts.init(dom);
+            },
+        }
     }
 </script>
 
 <style scoped>
 .el-card{
     height: 100%;
+}
+#dashLine{
+    width: 100%;
+    height:200px;
 }
 </style>
